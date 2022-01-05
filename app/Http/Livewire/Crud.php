ea123ab167cf604374ebddd3af3f5681abbc2295
enriquesousa <enrique.sousa@gmail.com>
2022-01-04 19:59:29 -0800

    21. CRUD Básico Parte 3

--- app/Http/Livewire/Crud.php
+++ app/Http/Livewire/Crud.php
@@ -8,9 +8,51 @@ use Livewire\WithPagination;
 class Crud extends Component
 {
     use WithPagination;
-
-    public $search, $action;
+    public $search, $action='Registrar';
+    public $name, $email, $password, $user_id=0, $hiddenpassword;
     protected $paginationTheme = 'bootstrap';
+
+    public function edit(User $user)
+    {
+        $this->user_id = $user->id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->password = null;
+        $this->hiddenpassword = $user->password;
+        $this->action = "Actualizar";
+        $this->dispatchBrowserEvent('modal', ['action' => 'show']);
+    }
+
+    public function store()
+    {
+        $user = User::updateOrCreate(
+            ['id' => $this->user_id],
+            [
+                'name' => $this->name,
+                'email' => $this->email,
+                'password' => strlen($this->password) > 0 ? bcrypt($this->password) : $this->hiddenpassword,
+            ]
+        );
+
+        $this->dispatchBrowserEvent('notify',
+            ['result' => $this->user_id > 0 ? 'Usuario Actualizado' : 'Usuario Registrado']
+        );
+
+        $this->reset('name', 'email', 'user_id', 'hiddenpassword', 'action', 'search');
+        $this->dispatchBrowserEvent('modal', ['action' => 'hide']);
+    }
+
+    protected $listeners = ['destroy'];
+    public function destroy(User $user){
+        $user->delete();
+        $this->dispatchBrowserEvent('notify', ['result' => 'Usuario Eliminado']);
+        $this->resetPage(); // reset paginado metodo de livewire
+    }
+
+    public function closeModal(){
+        $this->reset('name', 'email', 'user_id', 'hiddenpassword', 'action', 'search');
+        $this->dispatchBrowserEvent('modal', ['action' => 'hide']);
+    }
 
     public function render()
     {
